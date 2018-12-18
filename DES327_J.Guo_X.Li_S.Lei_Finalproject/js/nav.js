9648fefb2d9c94de03a7c676d36a210bc331a8bf
jessicacacaca <45408649+jessicacacaca@users.noreply.github.com>
2018-12-17 20:42:45 -0800

    Final_final

---
+++ DES327_J.Guo_X.Li_S.Lei_Finalproject/js/nav.js
@@ -0,0 +1,44 @@
+
+//burger menu button//
+
+function myFunction() {
+    var x = document.getElementById("menu_button");
+    if (x.className === "mainNav") {
+        x.className += " responsive";
+    } else {
+        x.className = "mainNav";
+    }
+    //document.getElementById("logo").style.display = "none";
+    //console.log("hide logo");
+}
+
+
+// photo slideshow for news and events//
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  console.log(slides.length + " " + slideIndex)
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
